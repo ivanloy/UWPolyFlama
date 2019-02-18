fbcdbfc84d8138758bd57ea7c5233c20d78084f8
Ignacio Van Loy <nachovanle@gmail.com>
2019-02-18 10:55:57 +0100

    GameView created

--- Monopoly - Cliente/PantallasMonopoly/Models/Propiedad.cs
+++ Monopoly - Cliente/PantallasMonopoly/Models/Propiedad.cs
@@ -3,7 +3,8 @@ using PantallasMonopoly.Models.Enums;
 using System;
 using System.Collections.Generic;
 using System.Linq;
-
+using Windows.UI;
+using Windows.UI.Xaml.Media;
 
 namespace PantallasMonopoly.Models
 {
@@ -12,8 +13,51 @@ namespace PantallasMonopoly.Models
         public int precio { get; set; }
         public int nCasas { get; set; }
         public bool tieneHotel { get; set; }
-        public Jugador comprador { get; set; }
+        public bool estaComprada { get; set; }
         public ColorPropiedad color { get; set; }
+
+        public Brush brushColor {
+            get {
+                Brush ret = new SolidColorBrush(Colors.Black);
+                if (this.estaComprada)
+                {
+                    switch (this.color)
+                    {
+                        case ColorPropiedad.AMARILLO:
+                            ret = new SolidColorBrush(Colors.Yellow);
+                            break;
+                        case ColorPropiedad.AZUL:
+                            ret = new SolidColorBrush(Colors.Blue);
+                            break;
+                        case ColorPropiedad.CELESTE:
+                            ret = new SolidColorBrush(Colors.AliceBlue);
+                            break;
+                        case ColorPropiedad.ESTACION:
+                            ret = new SolidColorBrush(Colors.White);
+                            break;
+                        case ColorPropiedad.MARRON:
+                            ret = new SolidColorBrush(Colors.Brown);
+                            break;
+                        case ColorPropiedad.NARANJA:
+                            ret = new SolidColorBrush(Colors.Orange);
+                            break;
+                        case ColorPropiedad.ROJO:
+                            ret = new SolidColorBrush(Colors.Red);
+                            break;
+                        case ColorPropiedad.ROSA:
+                            ret = new SolidColorBrush(Colors.Pink);
+                            break;
+                        case ColorPropiedad.SERVICIO:
+                            ret = new SolidColorBrush(Colors.LightGray);
+                            break;
+                        case ColorPropiedad.VERDE:
+                            ret = new SolidColorBrush(Colors.Green);
+                            break;
+                    }
+                }
+                return ret;
+            }
+        }
 
         public Propiedad(int precio, int nCasas, bool tieneHotel, Jugador comprador, ColorPropiedad color)
         {
---
+++ Monopoly - Cliente/PantallasMonopoly/ViewModels/GameViewModel.cs
@@ -0,0 +1,22 @@
+﻿using PantallasMonopoly.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PantallasMonopoly.ViewModels
+{
+    public class GameViewModel
+    {
+        private Jugador[] _jugadores;
+        
+        public Jugador[] Jugadores { get { return _jugadores; } set { _jugadores = value; } }
+
+        public GameViewModel()
+        {
+
+        }
+
+    }
+}
