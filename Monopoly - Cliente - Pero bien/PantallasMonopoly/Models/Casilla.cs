edbc0db30edae3bc9a49cffd3752d6a0dc65e3e4
Ignacio Van Loy <ignacio.van.loy@iesnervion.es>
2019-02-24 15:03:57 +0100

    LAS PIESA SE MUEVEN LOKO

--- Monopoly - Cliente - Pero bien/PantallasMonopoly/Models/Casilla.cs
+++ Monopoly - Cliente - Pero bien/PantallasMonopoly/Models/Casilla.cs
@@ -10,7 +10,14 @@ namespace PantallasMonopoly.Models
         public Uri imagen { get; set; }
         public TipoCasilla tipo { get; set; }
         public List<Jugador> listadoJugadores { get; set; }
-
+        public string tamanoImagenFichaJugador {
+            get {
+                string ret = "";
+                if (listadoJugadores.Count > 1) ret = "*";
+                else ret = "0";
+                return ret;
+            }
+        }
         public Casilla(TipoCasilla tipo, List<Jugador> listadoJugadores)
         {
             this.tipo = tipo;
--- Monopoly - Cliente - Pero bien/PantallasMonopoly/Models/Jugador.cs
+++ Monopoly - Cliente - Pero bien/PantallasMonopoly/Models/Jugador.cs
@@ -21,6 +21,7 @@ namespace PantallasMonopoly.Models
         public bool carcelGratisComunidad { get; set; }
         public bool estaEnCarcel { get; set; }
         public int turnosEnCarcel { get; set; }
+        public string dineroConSimboloDolar { get { return "$ " + this.dinero; } }
 
         public Jugador(string nombre, Ficha ficha, double dinero, List<Propiedad> listadoPropiedades, int posicion, bool carcelGratisSuerte, bool carcelGratisComunidad, bool estaEnCarcel, int turnosEnCarcel)
         {
